aadd80c897df42096c5493410b3b42b8ebfd35d2
cstuart756 <stujjc@yahoo.co.uk>
2025-12-15 15:51:49 +0000

    Fix syntax error in password validators

--- codestar/settings.py
+++ codestar/settings.py
@@ -90,4 +90,16 @@ DATABASES = {
 # Password validation
 # ------------------------------
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimil_
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
