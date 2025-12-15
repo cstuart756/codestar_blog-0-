6a2d243469fd3aaa7149049bd751b2fb828cce88
cstuart756 <stujjc@yahoo.co.uk>
2025-12-15 16:32:02 +0000

    Update codestar/urls.py to include blog.urls and remove old blog view import

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),  # root URL goes to blog app
+    path("", include("blog.urls"), name="blog-urls"),
 ]
 
 
