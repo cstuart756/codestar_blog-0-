df62d26cc5314044477e5fadc3c0a5e8e93b25d6
cstuart756 <stujjc@yahoo.co.uk>
2025-12-15 16:33:54 +0000

    Create PostList class-based view to display all blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
+from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
 class PostList(generic.ListView):
+    model = Post
     queryset = Post.objects.all()
     template_name = "blog/post_list.html"
