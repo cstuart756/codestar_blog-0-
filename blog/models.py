d4e9240abc67b437e66634898673976eade92cae
cstuart756 <stujjc@yahoo.co.uk>
2025-12-15 14:07:20 +0000

    add Post and Comment models

--- blog/models.py
+++ blog/models.py
@@ -15,8 +15,29 @@ class Post(models.Model):
         related_name="blog_posts"
     )
     content = models.TextField()
+    excerpt = models.TextField(blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="commenter"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author.username}: {self.body[:20]}"
