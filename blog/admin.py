2c42874841dde9d6faefe251e7de1723edaaba53
cstuart756 <stujjc@yahoo.co.uk>
2025-12-15 10:34:24 +0000

    Add Post model, admin registration, and initial migrations

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Post
 
+# Register your models here.
 admin.site.register(Post)
 
 
-# Register your models here.
+
