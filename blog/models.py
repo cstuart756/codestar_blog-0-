32bce59f088873b43b2c23a1f736ca85d201e2b9
cstuart756 <stujjc@yahoo.co.uk>
2025-12-16 09:01:14 +0000

    final tidy up

--- blog/models.py
+++ blog/models.py
@@ -1,32 +1,41 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User # Import models to connect
 
+STATUS = ((0, "Draft"), (1, "Published"))
+
+# Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+    
     def __str__(self):
-        return self.title
+        return f"{self.title} | written by {self.author}"
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
-        Post,  # now this works because Post is defined above
-        on_delete=models.CASCADE,
-        related_name="comments"
-    )
+        Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="commenter"
-    )
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    new_field = models.SlugField()
 
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment '{self.body}' by {self.author.username}"
+        return f"Comment {self.body} by {self.author}"
+        
