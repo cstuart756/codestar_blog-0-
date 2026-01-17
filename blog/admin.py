9ea0abda6b4ebac9813cfb6361ec3eeebe51aa1d
cstuart756 <stujjc@yahoo.co.uk>
2026-01-17 21:22:25 +0000

    Customize Django admin panel with Summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment
 
-# Register your models here.
-admin.site.register(Post)
+
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
-
-
-
