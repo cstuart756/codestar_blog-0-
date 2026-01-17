c18957117623d056b4046910a98cdbb560c9a9fb
cstuart756 <stujjc@yahoo.co.uk>
2026-01-17 20:43:58 +0000

    Add Comment model

--- blog/models.py
+++ blog/models.py
@@ -38,4 +38,16 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
+    
+    class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
         
