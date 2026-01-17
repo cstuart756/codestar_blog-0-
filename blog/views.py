bbb5a426af5ad48f170042cf1402fa4b7924b7c2
cstuart756 <stujjc@yahoo.co.uk>
2026-01-17 21:41:58 +0000

    Add base template, blog index template, and project static CSS

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,6 @@ from .models import Post
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.all()
-    template_name = "blog/post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
+
