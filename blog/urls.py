77a2d260b6024e08e1a6d94cd0ecd665928ac6fe
cstuart756 <stujjc@yahoo.co.uk>
2026-01-17 18:45:13 +0000

    Rename app to blog and fix URLs; ignore pycache

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
-from . import views
 from django.urls import path
+from .views import my_blog
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('', my_blog, name='blog'),
 ]
