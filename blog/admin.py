36e244fdae438225b297cfd712c0620a8565b4f5
cstuart756 <stujjc@yahoo.co.uk>
2026-01-18 17:01:27 +0000

    Update blog admin and models

--- blog/admin.py
+++ blog/admin.py
@@ -3,13 +3,21 @@ from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment
 
 
+@admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'slug', 'status', 'created_on')
-    search_fields = ['title', 'content']
-    list_filter = ('status', 'created_on')
-    prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('content',)
+    list_display = ("title", "slug", "status", "created_on")
+    search_fields = ["title", "content"]
+    list_filter = ("status", "created_on")
+    prepopulated_fields = {"slug": ("title",)}
+    summernote_fields = ("content",)
 
 
-admin.site.register(Post, PostAdmin)
-admin.site.register(Comment)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("post", "name", "approved", "created_on")
+    list_filter = ("approved", "created_on")
+    search_fields = ("name", "email", "body")
+    actions = ["approve_comments"]
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
