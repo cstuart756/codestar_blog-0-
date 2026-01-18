c9d00f389319a54f0bc925e01eab53606d094c45
cstuart756 <stujjc@yahoo.co.uk>
2026-01-18 13:31:30 +0000

    Add Cloudinary image support and fix Django settings

--- env.py
+++ env.py
@@ -5,3 +5,11 @@ os.environ.setdefault(
     "postgresql://neondb_owner:npg_9F2BYbNaEQcH@ep-morning-mountain-ageqosqw.c-2.eu-central-1.aws.neon.tech/amuse_most_rash_65920"
 )
 os.environ.setdefault("SECRET_KEY", "A597erx79!A597erx79!")
+import os
+
+os.environ.setdefault(
+    "CLOUDINARY_URL",
+    "cloudinary://<api_key>:cloudinary://118775343593858:cSXMIl2SljA4MSUJh0_hKKI3uf0@djkvgj6cz"
+)
+
+
